fc04ce068a1d1244ed6f39cde271b67710cf23e5
aNnAm2606 <73582949+aNnAm2606@users.noreply.github.com>
2022-09-25 23:26:26 +0200

    Window Events dispatching

--- Lanna/src/Lanna/Application.h
+++ Lanna/src/Lanna/Application.h
@@ -2,6 +2,8 @@
 
 #include "Core.h"
 #include "Events/Event.h"
+#include "Lanna/Events/ApplicationEvent.h"
+
 #include "Window.h"
 
 namespace Lanna {
@@ -13,7 +15,11 @@ namespace Lanna {
 		virtual ~Application();
 
 		void Run();
+
+		void OnEvent(Event& e);
 	private:
+		bool OnWindowClose(WindowCloseEvent& e);
+
 		std::unique_ptr<Window> m_Window;
 		bool m_Running = true;
 	};
