6806885735eaab019c7f062d1cd45b8b0a2c775b
Lau <lauraisi2000@gmail.com>
2022-09-26 23:12:29 +0200

    Input Polling

--- Lanna/src/Lanna/Application.cpp
+++ Lanna/src/Lanna/Application.cpp
@@ -5,6 +5,8 @@
 
 #include <glad/glad.h>
 
+#include "Input.h"
+
 namespace Lanna {
 
  #define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
@@ -62,6 +64,9 @@ namespace Lanna {
 
 			for (Layer* layer : m_LayerStack)
 				layer->OnUpdate();
+
+			auto [x, y] = Input::GetMousePosition();
+			LN_CORE_TRACE("{0}, {1}", x, y);
 
 			m_Window->OnUpdate();
 		}
